a5515e45f59aa2a28aa4aafb7ce5af46b4d2f9c0
maxmetodiev <max.metodiev.1999@gmail.com>
2026-01-25 00:43:57 +0200

    setup.bash modded

--- setup.bash
+++ setup.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.local/bin
+echo "Directory created at '$HOME/.local/bin'"
 # Ensure Flathub is enabled
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.discordapp.Discord com.valvesoftware.Steam com.visualstudio.code -y
